bf77ec4a4f535a8cfca7471576e5857c0feeed91
rails3-carma <rails3.carma@gmail.com>
2012-12-20 14:06:07 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paper::Application.config.secret_token = '1f661cd5e4e80fd0b617872965e91e016d9af17e7fc932150aba8c604d852b62a20556b205d391bf66e862129351d3932daa22c385f23fb26d27c27cc9feae34'
